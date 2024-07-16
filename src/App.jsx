6542f1a8589af737d56b1354f82a682928a40472
diegobermudez09 <diegobl9do@gmail.com>
2024-07-15 23:33:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+// Componente de React
+function App() {
+
+
+  
+  // es reservada para los templates o lo que quiero imprimir en la pantalla
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+
+    </div>
+  )
+}
+
+export default App
