93f839bf81a1ad9594f871c7825f83f34887d72e
diegobermudez09 <diegobl9do@gmail.com>
2024-07-16 00:18:56 -0500

    colocando props a los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from "./components/Header"
+import Button from "./components/Button"
 
 // Componente de React
 function App() {
@@ -16,11 +17,44 @@ function App() {
     setCantidad(+e.target.value);
   }
 
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
   // es reservada para los templates o lo que quiero imprimir en la pantalla
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
+      <div className="flex justify-between my-6">
+
+        <Button
+          operador='-'
+          fn={handleClickDecremento}
+          />
+        <Button
+          operador='+'
+          fn={handleClickIncremento}
+        />
+
+
+      </div>
       <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+
+// props se pasa en automatico en todos los componentes de react
+function Button(props) { // tambien puedo colocar distrocturing {operador, fn} y solo colocar operador
+    return (
+        <button
+            type='button'
+            className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full 
+            hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+            onClick={props.fn}
+        >{props.operador}</button>
+    )
+}
+
+export default Button
